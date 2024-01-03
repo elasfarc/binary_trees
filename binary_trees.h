483370eb60de5ad75f3b4ad1b6ff21fa47da8e77
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-01-03 05:04:44 +0200

    Add #binary_tree_node: creates a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,8 @@
+#ifndef _BINARY_TREE_
+#define _BINARY_TREE_
+
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - binary tree node
  *
@@ -8,10 +13,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
+
+
+void binary_tree_print(const binary_tree_t *tree);
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+#endif
