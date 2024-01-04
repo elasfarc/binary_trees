959c22033f137d4557d5727096ab01ca03d77c0c
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-01-04 12:05:59 +0200

    Add #binary_tree_height: calculates the height of a binary tree.

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+/**
+ * max - Find the maximum of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return:
+ * The maximum of the two integers.
+ */
+int max(int a, int b)
+{
+	return (a >= b ? a : b);
+}
+
+/**
+ * binary_tree_height - Calculate the height of a binary tree
+ * @tree: Pointer to the root node of the binary tree
+ *
+ * Description:
+ * This function calculates the height of a binary tree. The height of a binary
+ * tree is the length of the longest path from the root node to a leaf node.
+ *
+ * Return:
+ * The height of the binary tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
+	int node_height = 1 + max(left_height, right_height);
+
+	return (node_height);
+}
